9e4e814f453a79e9c9a0d9c9b5d7c211c372b28c
yo mismo <yo@mismo>
2023-01-30 18:40:24 +0100

    .

--- src/org/benigaslo/Main.java
+++ src/org/benigaslo/Main.java
@@ -1,5 +1,39 @@
+package org.benigaslo;
+
+
+import java.util.List;
+
+class Biblio {
+    String nom;
+    String city;
+    List<Llibre> llibres;
+
+    public Biblio(String nom, String city) {
+        this.nom = nom;
+        this.city = city;
+    }
+}
+
+class Llibre {
+    String nom;
+    int any;
+
+    Llibre(String elprmerdato, int elsegondato){
+        nom = elprmerdato;
+        any = elsegondato;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Llibre llibre1 = new Llibre("Libro1", 1980);
+        Llibre llibre2 = new Llibre("Libro2", 1200);
+        Llibre llibre3 = new Llibre("Libro3", 1200);
+
+        Biblio biblio1 = new Biblio("Biblio1", "La Vall");
+        Biblio biblio2 = new Biblio("Biblio1", "Fondeguilla");
+
+
+        biblio1.llibres.add(llibre1);
     }
 }
